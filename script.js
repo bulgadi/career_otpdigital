209ef67e1ee654ad57397d6646386667629ea44f
bulgadi <dima.shapoval@gmail.com>
2024-05-31 15:13:31 +0300

    8.1

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Дякуємо за ваше повідомлення. Ми скоро з вами зв'яжемося.');
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+    const options = {
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) return;
+            entry.target.style.opacity = 1;
+            entry.target.style.transform = 'translateY(0)';
+            observer.unobserve(entry.target);
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
 });
